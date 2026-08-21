970d63fb06daab189dadc87396808df44761db11
user3029469145 <3295680157715625-user3029469145@noreply.coze.cn>
2026-08-21 14:50:57 +0800

    Change next.config.ts to next.config.js

---
+++ next.config.js
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  allowedDevOrigins: ['*.dev.coze.site'],
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
